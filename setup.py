864f999d27863526bb1f6f56f651ccdf0f88fba5
Sainyam Kapoor <sainyamkapoor@yahoo.com>
2014-05-10 15:00:56 +0530

    Updated Setup from py2exe to cxfreeze

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
-from distutils.core import setup
+"""from distutils.core import setup
 import py2exe
 setup(console=['subtitle-downloader.py'])
+
+"""
+
+import sys
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "SubDown",
+    version = "0.1",
+    description = "subtitle-downloader",
+    executables = [Executable("subtitle-downloader.py", base = "Win32GUI")])
